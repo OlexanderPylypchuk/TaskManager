d8223b21ae32535f3e22d4dfff3d50a57495bbea
OleksanderPylypchuk <o.s.pylypchuk@student.khai.edu>
2024-09-02 09:52:37 +0300

    Add project files.

---
+++ TaskManager/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace TaskManager.Models
+{
+    public class User
+    {
+        [Guid]
+        [Key]
+        public int Id { get; set; }
+    }
+}
